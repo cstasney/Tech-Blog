866df0d67efc71d9cd7fd8679449d4134563cf89
cstasney <cstasney07@gmail.com>
2023-12-31 13:25:06 -0500

    get posts routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
-const { Post, Comment, User } = require('../models')
+const { Post, Comment, User } = require('../models');
+const { findByPk } = require('../models/User');
 
+// render all blog posts to the homepage
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({
@@ -14,5 +16,27 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// get desired blog post
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await findByPk(req.params.id, {
+            include: [User, {
+                model: Comment,
+                include: [User],
+            }]
+        })
+        if (postData) {
+            const post = postData.get({ plain: true });
+
+            res.render('single-post', {post});
+        } else {
+            res.status(404).end
+        }
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router;
