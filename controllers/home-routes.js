4244fcb900c29b885725b9db7680e9ebbc4803e1
cstasney <cstasney07@gmail.com>
2023-12-31 14:45:16 -0500

    sign up and sign in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,7 @@ router.get('/post/:id', async (req, res) => {
         if (postData) {
             const post = postData.get({ plain: true });
 
-            res.render('single-post', {post});
+            res.render('single-post', { post });
         } else {
             res.status(404).end
         }
@@ -38,5 +38,25 @@ router.get('/post/:id', async (req, res) => {
         res.status(500).json(err)
     }
 })
+
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
 
 module.exports = router;
