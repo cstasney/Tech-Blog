d8e67edfe7d0cb3f15dcf728c37a84f0370b3760
cstasney <cstasney07@gmail.com>
2024-01-01 16:23:20 -0500

    user id posting with new posts, delete and edit posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,8 +4,9 @@ const { update } = require('../../models/User');
 const router = require('express').Router();
 
 router.post('/', withAuth, async (req,res) => {
+    const body = req.body
     try {
-        const postData = await Post.create(req.body, { userId: req.session.userId});
+        const postData = await Post.create({ ...body, userId: req.session.userId});
         res.json(postData);
     } catch (err) {
         res.status(500).json(err)
@@ -22,6 +23,24 @@ router.put('/:id', withAuth, async (req, res) => {
       });
   
       if (updatedPost > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const postData = Post.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (postData) {
         res.status(200).end();
       } else {
         res.status(404).end();
