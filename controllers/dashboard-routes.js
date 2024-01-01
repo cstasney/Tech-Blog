7961d84ebb71c46dac56de52df64d76113f2a60c
cstasney <cstasney07@gmail.com>
2024-01-01 15:40:29 -0500

    new posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,27 +4,27 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
-      const postData = await Post.findAll({
-        where: {
-          userId: req.session.userId,
-        },
-      });
-  
-      const posts = postData.map((post) => post.get({ plain: true }));
-  
-      res.render('all-posts-admin', {
-        layout: 'dashboard',
-        posts,
-      });
-    } catch (err) {
-      res.redirect('login');
-    }
-  });
+        const postData = await Post.findAll({
+            where: {
+                userId: req.session.userId,
+            },
+        });
 
-  router.get('/new', withAuth, async (res, req) => {
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('all-posts-admin', {
+            layout: 'dashboard',
+            posts,
+        });
+    } catch (err) {
+        res.redirect('login');
+    }
+});
+
+router.get('/new', withAuth, (req, res) => {
     res.render('new-post', {
         layout: 'dashboard',
     });
-  });
+});
 
 module.exports = router;
